dce095facf02d18f2d530f46bacc58949de1020b
Julio Gonzalez <jusag87@gmail.com>
2022-08-29 02:47:28 -0300

    Terminando habilidades duras/blandas

--- src/app/components/hardysoft/edithardsoft.component.ts
+++ src/app/components/hardysoft/edithardsoft.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Hardsoft } from 'src/app/model/hardsoft';
+import { HardsoftService } from 'src/app/service/hardsoft.service';
 
 @Component({
   selector: 'app-edithardsoft',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edithardsoft.component.css']
 })
 export class EdithardsoftComponent implements OnInit {
-
-  constructor() { }
+  hardSoft:Hardsoft= null;
+  
+  constructor(private sHardSoft: HardsoftService, private activatedRouter: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.sHardSoft.detail(id).subscribe(
+      data => {
+        this.hardSoft = data;
+      }, err => {
+        alert("Error al updatear experiencia");
+        this.router.navigate(['']);
+      }
+    )
   }
-
+  onUpdate(): void {
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.sHardSoft.update(id, this.hardSoft).subscribe(
+      data => {
+        this.router.navigate(['']);
+      }, err => {
+        alert("Error al updatear Experiencia.");
+        this.router.navigate(['']);
+      }
+      )
+  }
 }
